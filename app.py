615bc58cc00102758dbed2a9254774f0d382ce45
SorelleKagam <sokag002@edu.cegepgarneau.ca>
2025-10-01 04:46:28 -0400

    remise en place du fichier apres suppression du lien git

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request,make_response, redirect
 app = Flask(__name__)
-
+def get_locale():
+    """ retourne la valeur du cookie s'il est enregistré """
+    return request.cookies.get('langue', 'fr')
 @app.route('/')
-
-def accueil():
-    return render_template ('accueil.jinja')
-
+def index():
+    """Récupère les paramètres régionaux pour afficher la page d'accueil"""
+    # Récupérer le cookie langue
+    langue = request.cookies.get('langue', 'fr')   
+    return render_template('Accueil.jinja')
+@app.route('/choisir_langue')
+def choisir_langue():
+    """ Modifie les paramètres régionaux """
+    langue = request.args.get("langue", default="fr")
+    response = make_response(redirect('/'))
+    response.set_cookie('langue', langue)
+    return response
 
 if __name__ == '__main__':
-    # Running the application in debug mode
     app.run(debug=True)
