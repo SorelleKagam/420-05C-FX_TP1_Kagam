7d4d76637b7735ced20c2866b50f9bda48b6c052
SorelleKagam <sokag002@edu.cegepgarneau.ca>
2025-10-02 21:48:31 -0400

    ajout des route s dans app.py

--- app.py
+++ app.py
@@ -1,5 +1,40 @@
+import random
 from flask import Flask, render_template, request,make_response, redirect
+
+
+import mysql.connector
+
+import bd
 app = Flask(__name__)
+
+def lister_routes():
+    """Liste les routes pour le menu"""
+    return [
+        {
+            'route' : '/',
+            'nom' : 'Accueil'
+        },
+        {
+            'route' : '/ajout',
+            'nom' : 'ajout'
+        },
+        {
+            'route' : '/services',
+            'nom' : 'services'
+        },
+        {
+            'route' : '/service',
+            'nom' : 'service'
+        },
+        {
+            'route' : '/edit-service',
+            'nom' : 'À propos'
+        },
+        {
+            'route' : '/confirmation',
+            'nom' : 'confirmation'
+        }
+    ]
 def get_locale():
     """ retourne la valeur du cookie s'il est enregistré """
     return request.cookies.get('langue', 'fr')
@@ -16,6 +51,54 @@ def choisir_langue():
     response = make_response(redirect('/'))
     response.set_cookie('langue', langue)
     return response
+
+@app.route('/ajout')
+def ajout():
+    """Affichage d'une liste"""
+
+    return render_template(
+        'ajout.jinja', titre_onglet="Ajout_service",
+        routes=lister_routes()
+    )
+    
+
+    @app.route('/services')
+def services():
+    """Affichage d'une liste"""
+    les_services = 
+    return render_template(
+        'services.jinja',
+        titre='Une liste!',
+        liste=les_services,
+        titre_onglet="services",
+        routes=lister_routes()
+    )
+
+@app.route('/sercice')
+def detail():
+    """Affichage d'une liste"""
+    return render_template(
+        'service.jinja',
+        titre_onglet="service",
+        routes=lister_routes()
+    )
+
+@app.route('/confirmation')
+def confirmation():
+    """Affichage d'une liste"""
+    return render_template(
+        'confirmation.jinja',
+        titre_onglet="confirmation",
+        routes=lister_routes()
+    )
+@app.route('/edit_service')
+def edit():
+    """Affichage d'une liste"""
+    return render_template(
+        'edit_service.jinja',
+        titre_onglet="modification",
+        routes=lister_routes()
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
